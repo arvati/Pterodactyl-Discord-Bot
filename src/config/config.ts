70f90beca99382f518769b2d7e930d989b9cbf14
Ademar Arvati <arvati@hotmail.com>
2021-07-14 13:22:30 -0300

    revert to original names

--- src/config/config.ts
+++ src/config/config.ts
@@ -18,7 +18,7 @@ export const config: Config = {
         // This is the lowest permisison level, this is for non-roled users.
         {
             level: 0,
-            name: 'USER',
+            name: 'User',
             // Don't bother checking, just return true which allows them to execute any command their
             // level allows them to.
             check: () => true,
@@ -27,7 +27,7 @@ export const config: Config = {
         {
             level: 2,
             // This is the name of the role.
-            name: 'MOD',
+            name: 'Moderator',
             // The following lines check the guild the message came from for the roles.
             // Then it checks if the member that authored the message has the role.
             // If they do return true, which will allow them to execute the command in question.
@@ -49,7 +49,7 @@ export const config: Config = {
         },
         {
             level: 3,
-            name: 'ADM',
+            name: 'Administrator',
             check: (message: Message): boolean => {
                 try {
                     const adminRole = message.guild?.roles.cache.find(
@@ -68,7 +68,7 @@ export const config: Config = {
         // This is the server owner.
         {
             level: 4,
-            name: 'OWNER',
+            name: 'Server Owner',
             // Simple check, if the guild owner id matches the message author's ID, then it will return true.
             // Otherwise it will return false.
             check: (message) =>
